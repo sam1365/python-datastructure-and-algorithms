91386bc3de1338ff5cf7b2efeff3616af07fcbc5
aisam <moradi.sayyedali@gmail.com>
2025-02-27 23:53:16 +0330

    feat: Added test_enqueue method To TestArrayQueue

--- tests/test_array_queue.py
+++ tests/test_array_queue.py
@@ -9,3 +9,13 @@ class TestArrayQueue:
 
     def test_is_empty(self):
         assert self.array_queue.is_empty() is True
+
+    def test_enqueue(self):
+        """Test enqueue elements in the array queue."""
+
+        self.array_queue.enqueue(1)
+        self.array_queue.enqueue(2)
+
+        assert self.array_queue.is_empty() is False
+        assert len(self.array_queue) == 2
+        assert self.array_queue.peek() == 1

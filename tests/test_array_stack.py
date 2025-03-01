ef939a8ec667e99980bb116af4f459db544ba81e
aisam <moradi.sayyedali@gmail.com>
2025-03-01 22:42:51 +0330

    feat: Changed TestArrayStack
    Added test_pop to it, to test pop method.

--- tests/test_array_stack.py
+++ tests/test_array_stack.py
@@ -19,3 +19,9 @@ class TestArrayStack:
         self.array_stack.push(5)
         assert self.array_stack.is_empty() is False
         assert self.array_stack.peek() == 5
+    def test_pop(self):
+        self.array_stack.push(1)
+        self.array_stack.push(2)
+        self.array_stack.push(3)
+        assert self.array_stack.pop() == 3
+        assert len(self.array_stack) == 2

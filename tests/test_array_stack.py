1e3e16494081ec1243472941227cc0bf982292ff
aisam <moradi.sayyedali@gmail.com>
2025-03-01 23:02:25 +0330

    feat: Changed TestArrayStack
    Added test_peek_is_empty to it, to test peek method when the array stack is empty.
    
    Added doc string to test_push, test_pop and test_peek_is_empty methods.

--- tests/test_array_stack.py
+++ tests/test_array_stack.py
@@ -10,18 +10,29 @@ class TestArrayStack:
 
     def test_is_empty(self):
         """Test is_empty function in the array stack."""
+
         assert self.array_stack.is_empty() is True
 
     def test_push(self):
+        """Test push function in the array stack."""
+
         self.array_stack.push(1)
         self.array_stack.push(2)
         self.array_stack.push(3)
         self.array_stack.push(5)
         assert self.array_stack.is_empty() is False
         assert self.array_stack.peek() == 5
+
     def test_pop(self):
+        """Test pop function in the array stack."""
+
         self.array_stack.push(1)
         self.array_stack.push(2)
         self.array_stack.push(3)
         assert self.array_stack.pop() == 3
         assert len(self.array_stack) == 2
+
+    def test_peek_is_empty(self):
+        """Test peek when stack is empty in the array stack."""
+        with pytest.raises(Exception, match="Stack is empty"):
+            self.array_stack.peek()

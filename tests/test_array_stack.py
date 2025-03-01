72593fa914d61cd2aa401357f1b9326d4e98255e
aisam <moradi.sayyedali@gmail.com>
2025-03-01 22:41:17 +0330

    feat: Changed TestArrayStack
    Added test_push to it, to test push method.

--- tests/test_array_stack.py
+++ tests/test_array_stack.py
@@ -11,3 +11,11 @@ class TestArrayStack:
     def test_is_empty(self):
         """Test is_empty function in the array stack."""
         assert self.array_stack.is_empty() is True
+
+    def test_push(self):
+        self.array_stack.push(1)
+        self.array_stack.push(2)
+        self.array_stack.push(3)
+        self.array_stack.push(5)
+        assert self.array_stack.is_empty() is False
+        assert self.array_stack.peek() == 5

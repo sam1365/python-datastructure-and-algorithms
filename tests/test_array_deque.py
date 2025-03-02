b67c5d35a9c4f950f41497083423e9d002f9a0b0
aisam <moradi.sayyedali@gmail.com>
2025-03-02 21:29:48 +0330

    feat: Changed TestArrayDeque Class
    Added test_add_front to test add_front method.

--- tests/test_array_deque.py
+++ tests/test_array_deque.py
@@ -11,3 +11,9 @@ class TestArrayDeque:
 
     def test_is_empty(self):
         assert self.array_deque.is_empty() is True
+
+    def test_add_front(self):
+        self.array_deque.add_front(1)
+        self.array_deque.add_front(2)
+        assert len(self.array_deque) == 2
+        assert self.array_deque.front() == 2

eed8d5dfd72c5025d55825eedcfbb3c0fa0edf63
aisam <moradi.sayyedali@gmail.com>
2025-03-02 23:15:44 +0330

    Changed LinkedStack class
    Added  docs for its methods.

--- stack/linked_stack.py
+++ stack/linked_stack.py
@@ -19,21 +19,31 @@ class LinkedStack:
         self._size = 0
 
     def __len__(self):
+        """Returns the size of the stack."""
+
         return self._size
 
     def is_empty(self):
+        """Returns whether the stack is empty."""
+
         return self._size == 0
 
     def push(self, value):
+        """Adds a node as head to the top of the stack."""
+
         self._head = Node(value, self._head)
         self._size += 1
 
     def peek(self):
+        """Returns the node data from the head of the stack."""
+
         if self._head is None:
             raise Exception('Stack is empty')
         return self._head.data
 
     def pop(self):
+        """Removes head and returns its data from the top of the stack."""
+
         if self.is_empty():
             raise Exception('Stack is empty')
         head_data = self._head.data

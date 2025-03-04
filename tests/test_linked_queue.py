af5cea9e6183b4ee74cacf0fb39e2e42bd47cb66
aisam <moradi.sayyedali@gmail.com>
2025-03-04 22:55:50 +0330

    feat: Changed TestLinkedQueue
    Added test_dequeue_is_empty.
    Added doc string to test_dequeue method.

--- tests/test_linked_queue.py
+++ tests/test_linked_queue.py
@@ -25,9 +25,15 @@ class TestLinkedQueue:
         assert len(self.linked_queue) == 3
 
     def test_dequeue(self):
+        """Test dequeue method for deleting an element from the queue."""
         self.linked_queue.enqueue(17)
         self.linked_queue.enqueue(13)
         self.linked_queue.enqueue(11)
         assert self.linked_queue.peek() == 17
         assert self.linked_queue.dequeue() == 17
         assert len(self.linked_queue) == 2
+
+    def test_dequeue_is_empty(self):
+        """Test dequeue method for deleting element from an empty queue."""
+        with pytest.raises(Exception, match='Queue is empty'):
+            self.linked_queue.dequeue()

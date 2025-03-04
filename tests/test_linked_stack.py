126a2c98ea3b25b4c6545d4be7c5ff84e820a9f3
aisam <moradi.sayyedali@gmail.com>
2025-03-04 19:17:57 +0330

    Changed TestLinkedStack
    Added docs in TestLinkedStack test functions.

--- tests/test_linked_stack.py
+++ tests/test_linked_stack.py
@@ -9,9 +9,13 @@ class TestLinkedStack:
         self.linked_stack = LinkedStack()
 
     def test_is_empty(self):
+        """Test is_empty function of LinkedStack class."""
+
         assert self.linked_stack.is_empty() is True
 
     def test_push(self):
+        """Test push function of LinkedStack class."""
+
         self.linked_stack.push(29)
         self.linked_stack.push(23)
         assert self.linked_stack.is_empty() is False
@@ -19,6 +23,8 @@ class TestLinkedStack:
         assert self.linked_stack.peek() == 23
 
     def test_pop(self):
+        """Test pop function of LinkedStack class."""
+
         self.linked_stack.push(29)
         self.linked_stack.push(23)
         assert self.linked_stack.is_empty() is False
@@ -26,5 +32,8 @@ class TestLinkedStack:
         assert self.linked_stack.pop() == 23
 
     def test_peek_is_empty(self):
+        """Test peek function of LinkedStack class
+        when there is an empty instance of the class"""
+
         with pytest.raises(Exception, match='Stack is empty'):
             self.linked_stack.peek()

82ecdf002255eddcf5c0a5edd176298ed268654c
aisam <moradi.sayyedali@gmail.com>
2025-03-05 22:49:37 +0330

    Changed TestCircularLinkedList
    Added doc strings to TestCircularLinkedList class methods.

--- tests/test_circular_linked_list.py
+++ tests/test_circular_linked_list.py
@@ -6,12 +6,19 @@ class TestCircularLinkedList:
 
     @pytest.fixture(autouse=True)
     def setup(self):
+        """Initialize a circular linked list for unit testing."""
+
         self.circular_linked_list = CircularLinkedList()
 
     def test_is_empty(self):
+        """Test is_empty method."""
+
         assert self.circular_linked_list.is_empty() is True
 
     def test_insert_rear(self):
+        """Test insert_rear method to
+        inserting new node at the rear."""
+
         self.circular_linked_list.insert_rear(1)
         self.circular_linked_list.insert_rear(2)
         self.circular_linked_list.insert_rear(3)
@@ -20,6 +27,8 @@ class TestCircularLinkedList:
         assert self.circular_linked_list.peek() == 1
 
     def test_remove_head(self):
+        """Test remove_head method to remove node at the head."""
+
         self.circular_linked_list.insert_rear(11)
         self.circular_linked_list.insert_rear(13)
         self.circular_linked_list.insert_rear(17)
@@ -30,5 +39,7 @@ class TestCircularLinkedList:
         assert self.circular_linked_list.peek() == 13
 
     def test_remove_head_for_empty_list(self):
+        """Test remove_head for empty list."""
+
         with pytest.raises(Exception, match='Empty List'):
             self.circular_linked_list.remove_head()

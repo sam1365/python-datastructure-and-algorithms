a57c8f035f397aec3ded2704fb2847a872bc1ce4
aisam <moradi.sayyedali@gmail.com>
2025-03-09 20:54:12 +0330

    docs: Changed TestLinkedDeque
    Added doc string to method of it.

--- tests/test_linked_deque.py
+++ tests/test_linked_deque.py
@@ -6,12 +6,18 @@ class TestLinkedDeque:
 
     @pytest.fixture(autouse=True)
     def setup(self):
+        """Initialize a linked deque for unit testing."""
+
         self.linked_deque = LinkedDeque()
 
     def test_is_empty(self):
+        """Test is_empty method."""
+
         assert self.linked_deque.is_empty() is True
 
     def test_add_front(self):
+        """Test add_front method to add front node to deque."""
+
         assert self.linked_deque.is_empty() is True
         self.linked_deque.add_front(21)
         assert self.linked_deque.is_empty() is False
@@ -19,6 +25,8 @@ class TestLinkedDeque:
         assert self.linked_deque.front() == 21
 
     def test_add_rear(self):
+        """Test add_rear method to add rear node to deque."""
+
         assert self.linked_deque.is_empty() is True
         self.linked_deque.add_rear(21)
         self.linked_deque.add_rear(23)
@@ -27,6 +35,8 @@ class TestLinkedDeque:
         assert self.linked_deque.rear() == 23
 
     def test_remove_front(self):
+        """Test remove_front method to remove front node from deque."""
+
         assert self.linked_deque.is_empty() is True
         self.linked_deque.add_front(21)
         self.linked_deque.add_front(23)
@@ -37,6 +47,8 @@ class TestLinkedDeque:
         assert self.linked_deque.front() == 23
 
     def test_remove_rear(self):
+        """Test remove_rear method to remove rear node from deque."""
+
         assert self.linked_deque.is_empty() is True
         self.linked_deque.add_rear(21)
         self.linked_deque.add_rear(23)
@@ -47,6 +59,8 @@ class TestLinkedDeque:
         assert self.linked_deque.front() == 21
 
     def test_clear_deque(self):
+        """Test clear_deque method to clear deque."""
+
         assert self.linked_deque.is_empty() is True
         self.linked_deque.add_rear(21)
         self.linked_deque.add_rear(23)

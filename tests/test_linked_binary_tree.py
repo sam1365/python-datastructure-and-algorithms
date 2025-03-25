315b99750e25a70d736b736f3f0da0a9bcc987a1
aisam <moradi.sayyedali@gmail.com>
2025-03-25 22:36:17 +0330

    feat: Changed TestLinkedBinaryTree
    Added test_add_right method to test add_right method.

--- tests/test_linked_binary_tree.py
+++ tests/test_linked_binary_tree.py
@@ -39,3 +39,13 @@ class TestLinkedBinaryTree:
         assert isinstance(self.linked_binary_tree.left(self.linked_binary_tree.root()),
                           BinaryTreePosition)
         assert self.linked_binary_tree.left(self.linked_binary_tree.root()).element() == 19
+
+    def test_add_right(self):
+        assert self.linked_binary_tree.root() is None
+        self.linked_binary_tree.add_root(17)
+        assert self.linked_binary_tree.root() is not None
+        self.linked_binary_tree.add_right(self.linked_binary_tree.root(), 23)
+        assert self.linked_binary_tree.right(self.linked_binary_tree.root()) is not None
+        assert isinstance(self.linked_binary_tree.right(self.linked_binary_tree.root()),
+                          BinaryTreePosition)
+        assert self.linked_binary_tree.right(self.linked_binary_tree.root()).element() == 23

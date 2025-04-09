79f65bd9bd9712567941dcd8b87e0713a0a0a67d
aisam <moradi.sayyedali@gmail.com>
2025-04-09 16:20:06 +0330

    feat: Changed TestPositionalList
    Added test_add_first method to test add_first method.

--- tests/test_positional_list.py
+++ tests/test_positional_list.py
@@ -12,3 +12,10 @@ class TestPositionalList:
         """Initialize an instance of PositionalList Class for testing purpose."""
 
         self.positional_list = PositionalList()
+
+    def test_add_first(self):
+        assert len(self.positional_list) == 0
+        self.positional_list.add_first(31)
+        assert len(self.positional_list) == 1
+        self.positional_list.first().element() == 31
+        

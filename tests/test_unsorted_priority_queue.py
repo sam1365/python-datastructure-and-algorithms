25b92352732a9c4722e964f9d812396ceb6714c1
aisam <moradi.sayyedali@gmail.com>
2025-04-09 21:52:35 +0330

    feat: Changed TestUnsortedPriorityQueue
    Added test_min to test if remove_min method removes correct item from the priority queue.

--- tests/test_unsorted_priority_queue.py
+++ tests/test_unsorted_priority_queue.py
@@ -18,6 +18,7 @@ class TestUnsortedPriorityQueue:
         Test if min method in UnsortedPriorityQueue
         return correct min key
         """
+
         self.priority_queue.add(4, 'D')
         self.priority_queue.add(5, 'E')
         self.priority_queue.add(1, 'A')
@@ -25,3 +26,19 @@ class TestUnsortedPriorityQueue:
         self.priority_queue.add(3, 'C')
         min = self.priority_queue.min()
         assert min == (1, 'A')
+
+    def test_remove_min(self):
+        """
+        Test if remove_min method in UnsortedPriorityQueue
+        remove correct item with min key
+        """
+
+        self.priority_queue.add(4, 'D')
+        self.priority_queue.add(5, 'E')
+        self.priority_queue.add(1, 'A')
+        self.priority_queue.add(2, 'B')
+        self.priority_queue.add(3, 'C')
+        old_min = self.priority_queue.remove_min()
+        assert old_min == (1, 'A')
+        min_item = self.priority_queue.min()
+        assert min_item == (2, 'B')

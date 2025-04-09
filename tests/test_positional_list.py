ba0cb5e74ac1ec510f83119048e93940dd6c8365
aisam <moradi.sayyedali@gmail.com>
2025-04-09 16:32:50 +0330

    feat: Changed TestPositionalList
    Added test_after method to test after method.

--- tests/test_positional_list.py
+++ tests/test_positional_list.py
@@ -44,3 +44,15 @@ class TestPositionalList:
         self.positional_list.add_last(37)
         assert len(self.positional_list) == 2
         self.positional_list.before(self.positional_list.last()).element() == 31
+
+    def test_before(self):
+        """
+        Test after method of PositionalList.
+        """
+
+        assert len(self.positional_list) == 0
+        self.positional_list.add_first(31)
+        self.positional_list.add_last(37)
+        assert len(self.positional_list) == 2
+        self.positional_list.after(self.positional_list.first()).element() == 37
+

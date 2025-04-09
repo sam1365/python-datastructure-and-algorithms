498ac274ae0816b6db0e48729caaffa2df7cb8ad
aisam <moradi.sayyedali@gmail.com>
2025-04-09 20:12:16 +0330

    feat: Changed TestUnsortedPriorityQueue
    Added test_add method to test adding an item to the test instance of UnsortedPriorityQueue class.

--- tests/test_unsorted_priority_queue.py
+++ tests/test_unsorted_priority_queue.py
@@ -7,4 +7,8 @@ class TestUnsortedPriorityQueue:
     @pytest.fixture(autouse=True)
     def setup(self):
         self.priority_queue = UnsortedPriorityQueue()
-    
+
+    def test_add(self):
+        assert len(self.priority_queue) == 0
+        self.priority_queue.add(1, 'A')
+        assert len(self.priority_queue) == 1

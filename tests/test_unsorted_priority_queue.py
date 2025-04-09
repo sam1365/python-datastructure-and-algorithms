57275432b3ade43d8041b353f726fb99b598bbe4
aisam <moradi.sayyedali@gmail.com>
2025-04-09 20:37:25 +0330

    feat: Changed TestUnsortedPriorityQueue
    Added test_min to test if min method return correct minimum key in the priority queue.

--- tests/test_unsorted_priority_queue.py
+++ tests/test_unsorted_priority_queue.py
@@ -12,3 +12,16 @@ class TestUnsortedPriorityQueue:
         assert len(self.priority_queue) == 0
         self.priority_queue.add(1, 'A')
         assert len(self.priority_queue) == 1
+
+    def test_min(self):
+        """
+        Test if min method in UnsortedPriorityQueue
+        return correct min key
+        """
+        self.priority_queue.add(4, 'D')
+        self.priority_queue.add(5, 'E')
+        self.priority_queue.add(1, 'A')
+        self.priority_queue.add(2, 'B')
+        self.priority_queue.add(3, 'C')
+        min = self.priority_queue.min()
+        assert min == (1, 'A')

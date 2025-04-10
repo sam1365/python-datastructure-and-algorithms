737c6405327e7cd2efff6993f828bfd85d865091
aisam <moradi.sayyedali@gmail.com>
2025-04-10 19:41:22 +0330

    feat: Added test_remove_min_raises_error_when_queue_is_empty method to TestUnsortedPriorityQueue
    Its responsible to check if remove_min raises correct message
    when the priority queue is empty or not.

--- tests/test_sorted_priority_queue.py
+++ tests/test_sorted_priority_queue.py
@@ -40,3 +40,24 @@ class TestUnsortedPriorityQueue:
         self.priority_queue.add(2, 'B')
         removed_item = self.priority_queue.remove_min()
         assert removed_item == (1, 'A')
+
+    def test_remove_min_(self):
+        """
+        Test if remove_min method in SortedPriorityQueue
+        remove and return correct min item based on minimum key
+        of the items in the priority queue
+        """
+
+        self.priority_queue.add(1, 'A')
+        self.priority_queue.add(2, 'B')
+        removed_item = self.priority_queue.remove_min()
+        assert removed_item == (1, 'A')
+
+    def test_remove_min_raises_error_when_queue_is_empty(self):
+        """
+        Test remove_min raises error when priority queue is empty.
+        """
+
+        with pytest.raises(ValueError) as error_info:
+            self.priority_queue.remove_min()
+        assert str(error_info.value) == "Priority queue is empty"

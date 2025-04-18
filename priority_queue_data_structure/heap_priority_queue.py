9a994b5a9bf625abdea91ebe30e6c7fcca355301
aisam <moradi.sayyedali@gmail.com>
2025-04-18 22:42:43 +0330

    feat: Develeoped HeapPriorityQueue Class
    It is an implementation of min-oriented priority queue  using binary heap.

---
+++ priority_queue_data_structure/heap_priority_queue.py
@@ -0,0 +1,9 @@
+from priority_queue_data_structure.priority_queue_data_structure import BasePriorityQueue
+
+
+class HeapPriorityQueue(BasePriorityQueue):
+    """
+    A min-oriented priority queue implementation by using binary heap.
+    """
+    def __init(self):
+        self._data = []
